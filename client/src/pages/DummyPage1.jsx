11a3ab717755ee9bd8ba1725c32ae34c3c9932cb
Edward-Chen03 <nyc.chen.edward@gmail.com>
2024-03-30 14:47:39 -0400

    Added Frontend and Backend Connection

--- client/src/pages/DummyPage1.jsx
+++ client/src/pages/DummyPage1.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import axios from 'axios';
 export default function HomePage() {
     let navigate = useNavigate();
 
@@ -7,8 +7,25 @@ export default function HomePage() {
         navigate('/')
     }
 
-    let testBack = () => {
+    let testBack = async () => {
+
+        let testIngredients = ["Butter", "Chicken", "Potato", "Cilantro"]
         
+        await axios.post('http://localhost:3000/updateIngredients', {
+
+            ingredients: testIngredients
+
+        })
+
+    }
+
+    let testBack1 = async () => {
+
+        
+        let test = await axios.get('http://localhost:3000/getNewRecipe');
+
+        console.log(test);
+
     }
 
     return (
@@ -17,6 +34,8 @@ export default function HomePage() {
         <button onClick={homePage}> Test Home </button>
 
         <button onClick = {testBack}>Test Backend</button>
+
+        <button onClick = {testBack1}>Test Backend1</button>
 
         </>
         
--- server/server.js
+++ server/server.js
@@ -14,49 +14,67 @@ app.use(cors());
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 const openai = new OpenAI();
 
-let conversation =  [{ role: "system", content: "You are a chef creating recipes for me based only on the ingredients in their kitchen and their fitness goals" },
-                  {role: "assistant", content: "I currently have no ingredients in my kitchen and no fitness goals"}]
+let conversation = [{ role: "system", content: "You are a chef creating recipes for me based only on the ingredients in their kitchen and their fitness goals" },
+{ role: "assistant", content: "I currently have no ingredients in my kitchen and no fitness goals" }]
 
 async function generateCompletion(conversation) {
-    try {
-      const completion = await openai.chat.completions.create({
-        messages: conversation,
-        model: "gpt-3.5-turbo",
-      });
-      //handle completion
-      console.log(completion.choices[0]);
-    } catch (error) {
-      console.error("Error: " + error)
-    }
-    
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: conversation,
+      model: "gpt-3.5-turbo",
+    });
+    //handle completion
+    console.log(completion.choices[0]);
+
+    return completion.choices[0]
+
+  } catch (error) {
+    console.error(error)
   }
-  
-  app.post('/updateIngredients', (req, res) => {
-    //request is an array of ingredients
-    update = "I now have these ingredients in my kitchen: "
-    for(ingredient in req.body.ingredients){
-      update = update + ingredient + ", ";
+
+}
+
+app.post('/updateIngredients', (req, res) => {
+  //request is an array of ingredients
+  console.log(req.body.ingredients);
+  const ingredients = req.body.ingredients;
+  update = "I now have these ingredients in my kitchen: "
+  for (let i = 0; i < ingredients.length; i++) {
+    if (i == ingredients.length - 1) {
+      update += ingredients[i]
     }
-    conversation.push(update);
-    res.send("ingredients have been updated");
-  })
+    else {
+      update += ingredients[i] + ", ";
+    }
+  }
 
-  app.post('/updateFitnessGoal', (req, res) => {
-    //request is an array of ingredients
-    update = "My fitness goal is now: " + req.body.goal;
-    conversation.push(update);
-    res.send("Fitness Goal has been updated");
-  })
 
-  app.get('/getNewRecipe', async (req, res) => {
-    completion = await generateCompletion(conversation);
-    res.send(completion.choices[0].message.content);
-  })
+
+  console.log(update);
+  newConvoEntry = {role: "assistant", content: update};
+  conversation.push(newConvoEntry);
+  res.send("ingredients have been updated");
+})
+
+app.post('/updateFitnessGoal', (req, res) => {
+  //request is an array of ingredients
+  update = "My fitness goal is now: " + req.body.goal;
+  console.log(update);
+  conversation.push(update);
+  res.send("Fitness Goal has been updated");
+})
+
+app.get('/getNewRecipe', async (req, res) => {
+
+  completion = await generateCompletion(conversation);
+  console.log(completion);
+  res.send(completion.message.content);
+})
 
 
 
