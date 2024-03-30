df41cb009e3ea845962e002e1a49a89f25b448cc
Marcus Lau <108362808+MarcusLau1030@users.noreply.github.com>
2024-03-30 17:36:22 -0400

    Ingredient Search and chip

--- client/src/pages/IngredientsPage/IngredientsPage.jsx
+++ client/src/pages/IngredientsPage/IngredientsPage.jsx
@@ -1,13 +1,27 @@
+import * as React from 'react';
 import SideBar from "../../components/SideBar/SideBar"
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper"
+import IngredientSearch from "../../components/IngredientSearch/IngredientSearch"
+import './IngredientsPage.css'
+import IngredientChip from "../../components/IngredientChip/IngredientChip"
 
 export default function Ingredients() {
+
+    const [showChip, setShowChip] = React.useState(true);
+
+    const handleDelete = () => {
+        setShowChip(false);
+    };
+
     return(
         <>
         <span style={{display: "flex"}}>
             <SideBar></SideBar>
             <ContentWrapper>
-                INGREDIENTS
+                <span className="IngredientsContent">
+                    <IngredientSearch></IngredientSearch>
+                </span>
+
             </ContentWrapper>
         </span>
         </>
