1ada024dcddd7fc9170080decd05b7ecf2ff2107
Edward-Chen03 <nyc.chen.edward@gmail.com>
2024-03-31 00:53:06 -0400

    Merge Fix

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -9,14 +9,34 @@ export default function HomePage() {
     const location = useLocation();
     const navigate = useNavigate();
     const { emailChange, username } = location.state || {};
+    let [usersList, setUsersList] = useState([]);
+
 
     console.log(emailChange);
 
-    useEffect(() => {
+    useEffect(async () => {
 
         if (!emailChange) {
             navigate('/');
         }
+
+        await axios.get('http://localhost:3000/users')
+            .then(res => { setUsersList(res.data); })
+            .catch(err => console.error(err));
+
+        let findUser = usersList.find(user => user.email === emailChange);
+
+        await axios.post('http://localhost:3000/updatePersonalInformation', {
+
+            goal: findUser.fitness,
+            weight: findUser.weight,
+            age: findUser.age,
+            gender: findUser.gender
+
+        }).then(
+            //made create a popup to tell people that there information was updated
+            console.log("Settings have been updated")
+        );
 
     }, [emailChange, navigate]);
 
